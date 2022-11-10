dd674ad55f70314bc8a826938bcd4abcde4e0e94
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2022-11-10 23:26:08 +0530

    fricking did it- somehow managed the posts page

--- posts/admin.py
+++ posts/admin.py
@@ -7,10 +7,10 @@ from .models import Post
 # to show full details of each post on admin panel
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('title', 'slug', 'author', 'publish', 'status')
-	list_filter = ('status', 'created', 'publish', 'author')
-	search_fields = ('title', 'body')
-	prepopulated_fields = {'slug': ('title',)}
-	raw_id_fields = ('author',)
-	data_hierarchy = 'publish'
-	ordering = ('status', 'publish')
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("status", "created", "publish", "author")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    data_hierarchy = "publish"
+    ordering = ("status", "publish")
